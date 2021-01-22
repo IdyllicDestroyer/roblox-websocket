5398d83c781fef6e65e07e83b6ed06370bed9e0c
tonumber <77855848+tonumber@users.noreply.github.com>
2021-01-22 17:18:51 -0500

    Add files via upload

---
+++ WebSocket.lua
@@ -0,0 +1,152 @@
+-- This is a modulescript named WebSocket (put in workspace)
+local API = {
+	Config = {},
+};
+local placeholder = function()end;
+--// Events
+API.onopen = placeholder;
+API.onclose = placeholder;
+API.onmessage = placeholder;
+local jsonDecode = function(...) return game:GetService('HttpService'):JSONDecode(...); end;
+local jsonEncode = function(...) return game:GetService('HttpService'):JSONEncode(...); end;
+function API.HttpRequest(U, M, H, B)
+  return game:GetService('HttpService'):RequestAsync({
+    Url = U,
+    Method = M or "GET",
+    Headers = H,
+    Body = B
+   })
+end
+function API.Request(Path, Method, Headers, Body)
+	
+	local Data = API.HttpRequest(API.Config.Host .. ":" .. API.Config.Port .. Path, Method, Headers or {
+		Authentication = API.Config.Authentication
+	}, Body).Body;
+	return jsonDecode(Data);
+	
+end
+
+function API.Setup(Host, Port, Authentication)
+	API.Config = {
+		Host = Host,
+		Port = Port,
+		Authentication = Authentication
+	}
+	
+end
+function API.IsConnected()
+	local data = API.Request("/connection/get");
+	if (data.data == false) then	
+		return false;
+		
+	else	
+		return true;	
+	end
+	
+end
+function API.GetConnection()
+	return API.Request("/connection/get").data;
+end
+function API.Connect(Url)
+	local data = API.Request("/connect/" .. game:GetService'HttpService':UrlEncode(Url), "POST");
+	if (data.success) then
+		API.onopen();
+		return true;
+	else
+		return false, data.reason;
+	end
+	
+end
+function API.Disconnect()
+	local data = API.Request("/disconnect", "POST");
+	if (data.success) then
+		return true;
+	else
+		return false, data.reason;
+	end
+end
+
+function API.Ping()
+	return API.Request("/connection/ping", "POST").success;
+end
+
+function API.Send(message)
+	local data = API.Request("/connection/send", "POST", {
+		Authentication = API.Config.Authentication;
+		["Content-Type"] = "application/json";
+	}, jsonEncode{
+		Content = message,
+	});
+	if (data.success) then
+		return true
+	else
+		return false, data.reason;
+	end
+end
+
+function API.GetMessages()
+	local data = API.Request("/connection/messages");
+	if (data.success) then
+		
+		return true, data.data;
+		
+	else
+		
+		return false, data.reason;
+		
+	end
+	
+end
+
+--// Listen
+local isListening = false;
+local listenTime = .1;
+
+function API.StartListen(Interval)
+	
+	listenTime = Interval or 1;
+	isListening = true;
+	
+end
+
+spawn (function()
+	while wait(listenTime) do
+		
+		local Suc, Error = pcall(function()
+			
+			if (isListening) then
+				
+				local success, messages = API.GetMessages();
+				
+				if (success) then
+					
+					if (#messages > 0) then
+						
+						table.foreach(messages, function (_, msg) 
+							API.onmessage(msg);
+						end);
+						
+					end
+					
+				else
+					
+					API.onclose();
+					isListening = false;
+					
+				end
+				
+			end
+			
+		end)
+		
+		if (not Suc) then
+			
+			warn("Error >:(", Error);
+			wait(2);
+			
+		end
+		
+	end
+end)
+
+return API;
---
+++ examples/discord_bot.lua
@@ -0,0 +1,65 @@
+local botToken = "";
+local status = "discord bot hosted on roblox";
+local WS = require(workspace.WebSocket);
+WS.Setup("http://your.ip.here", 2030); -- port forward it you dumby. netgear users go to routerlogin.net
+local Heartbeat = 0;
+local packets = {
+	["1"] = function() 
+		WS.Send('{"op": 1, "d": null}');
+	end,
+	["2"] = function() 
+		WS.Send(game:GetService('HttpService'):JSONEncode({
+			op = 2,
+			d = {
+				token = "Bot " .. BotToken,
+				properties = {
+					["$os"] = "windows",
+					["$browser"] = "roblox",
+					["$device"] = "roblox"
+				},
+				
+				presence = {
+					status = "online",
+					game = {
+						name = status,
+						type = 0
+					}
+				}
+			}
+		}))
+	end,
+}
+WS.onopen = function()
+	print('Discord Bot started.')
+end
+WS.onclose = function()
+	print('Closed')
+end
+WS.onmessage = function(...)
+	local Decoded = game:GetService('HttpService'):JSONDecode(...);
+	local Event = Decoded.t;
+	local Data = Decoded.d;
+	if (Decoded.op == 10) then 
+		Heartbeat = Decoded.d.heartbeat_interval / 1000;
+	elseif (Decoded.op == 1) then 
+		packets["1"]();	
+	elseif (Event == "MESSAGE_CREATE") then
+		print(Data.author.username .. "#" .. Data.author.discriminator .. ": " .. Data.content);
+	end
+end
+if (WS.IsConnected()) then
+	WS.Disconnect();
+end
+WS.StartListen(1);
+WS.Connect("wss://gateway.discord.gg");
+packets['2']();
+spawn(function()
+   while wait() do
+	if (Heartbeat == 0) then
+	    wait(.1);
+	else
+	    wait(Heartbeat);
+	    packets["1"]();
+	end
+    end
+end)
